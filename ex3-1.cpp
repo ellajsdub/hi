5718b307a906992a71b9d7545c3e2b2151211615
ellajsdub <104838506+ellajsdub@users.noreply.github.com>
2022-05-10 21:09:55 +0800

    Create ex3-1.cpp

---
+++ ex3-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x,y;
+    printf("please input x: \n"); 
+    scanf("%d",&x); 
+    
+    printf("please input y: \n"); 
+    scanf("%d",&y); 
+    
+    printf(" %d + %d %d \n ",x,y,x+y);
+    
+    cout<<"Hello World"; 
+    
+    return 0; 
+    
+}
